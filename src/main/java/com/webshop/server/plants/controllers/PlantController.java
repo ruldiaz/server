1c2ed8284449cff5fd171fe498bb4a716040711b
RaulDiaz <rulhdiaz@gmail.com>
2024-02-13 15:28:44 -0600

    commit

--- src/main/java/com/webshop/server/plants/controllers/PlantController.java
+++ src/main/java/com/webshop/server/plants/controllers/PlantController.java
@@ -3,16 +3,10 @@ package com.webshop.server.plants.controllers;
 import com.webshop.server.plants.entities.Plant;
 import com.webshop.server.plants.repositories.PlantRepository;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("api/v1/customers")
 public class PlantController {
     private final PlantRepository plantRepository;
 
@@ -23,6 +17,11 @@ public class PlantController {
     @GetMapping()
     public String getMessage() {
         return "Hello";
+    }
+
+    @GetMapping("/error")
+    public String getError() {
+        return "Error";
     }
     @GetMapping("/plants")
     public Iterable<Plant> getAllPlants() {
